a04bbfb7e969d7a53d6d39bf9b23849989d83a00
Rikard Mustajärvi <rikard.mustajarvi@gmail.com>
2014-11-26 16:47:06 +0100

    Added simple helper for codahale Metrics

---
+++ src/main/scala/com/svartsjo/scalautil/MetricsHelper.scala
@@ -0,0 +1,32 @@
+package com.svartsjo.scalautil
+
+import java.util.concurrent.TimeUnit
+import com.codahale.metrics.ConsoleReporter
+import com.codahale.metrics.MetricRegistry
+import com.codahale.metrics.Timer
+
+trait MetricsHelper {
+  import com.codahale.metrics._
+
+  val metricsRegistry = new MetricRegistry()
+
+  private def name(names: String*) = MetricRegistry.name(this.getClass(), names: _*)
+
+  // checked metric code, looks to be thread safe
+  def getMeter(names: String*) = metricsRegistry.meter(name(names: _*))
+  def getTimer(names: String*) = metricsRegistry.timer(name(names: _*))
+  def getCounter(names: String*) = metricsRegistry.counter(name(names: _*))
+
+  def time[A](f: => A, timer: Timer): A = {
+    val context = timer.time()
+    try {
+      f
+    } finally {
+      context.stop()
+    }
+  }
+
+  def startSimpleConsoleReporter() {
+    ConsoleReporter.forRegistry(metricsRegistry).build().start(5, TimeUnit.SECONDS)
+  }
+}
